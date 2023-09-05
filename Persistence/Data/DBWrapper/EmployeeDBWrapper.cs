df91a59e708f5f3f208a5c75f7ecc75e530519f6
propellis <prinsessellis@gmail.com>
2023-09-05 10:10:09 +0200

    Data hardkodet i dbwrapper

--- Persistence/Data/DBWrapper/EmployeeDBWrapper.cs
+++ Persistence/Data/DBWrapper/EmployeeDBWrapper.cs
@@ -12,7 +12,34 @@ namespace Persistence.Data.DBWrapper
         }
         public List<Employee> EmployeeListGet()
         {
-            return _employeeContext.Employees.ToList();
+           
+            // return _employeeContext.Employees.ToList();
+           var employeesList = new List<Employee>();
+            employeesList.Add(new Employee() {
+                Id = 1,
+                Fornavn = "Lykke",
+                Etternavn = "Liten",
+                Adresse = "Kjærlighetsstien 6",
+                Telefonnr = "66 66 66 66"
+                }
+            );
+            employeesList.Add(new Employee() {
+                Id = 2,
+                Fornavn = "Hoppsi",
+                Etternavn = "Deisi",
+                Adresse = "Hoppegåsveien 8",
+                Telefonnr = "88 88 88 88"
+                }
+            );
+            employeesList.Add(new Employee() {            
+                Id = 3,
+                Fornavn = "Boppi",
+                Etternavn = "Loppi",
+                Adresse = "Schvoppegata 16",
+                Telefonnr = "16 16 16 16"
+                }
+            );
+            return employeesList;
         }
     }
 }
